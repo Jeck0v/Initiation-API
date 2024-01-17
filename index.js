aedea2d1f91e1f777ccdd2090f7d7be80a389cce
Arnaud F <149910271+Jeck0v@users.noreply.github.com>
2024-01-17 16:57:34 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+// npm install express
+// npm init -y  
+// node index.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log(req.query.name);
+  res.send('Salut, monde du développement !');
+});
+app.get('/hello', (req, res) => {
+    const name = req.query.name;
+    res.send(`Hello, ${name}!`);
+});
+  
+app.listen(3000, () => {
+  console.log('Serveur démarré sur http://localhost:3000');
+});
+
