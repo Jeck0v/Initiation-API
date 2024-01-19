ce68e281128729c4a7fa55abfc9e51d8ff2dbcea
Arnaud F <149910271+Jeck0v@users.noreply.github.com>
2024-01-19 19:12:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // npm install express
 // npm init -y  
 // node index.js
+
 const express = require('express');
 const app = express();
-
 app.get('/', (req, res) => {
     console.log(req.query.name);
   res.send('Salut, monde du développement !');
@@ -12,7 +12,6 @@ app.get('/hello', (req, res) => {
     const name = req.query.name;
     res.send(`Hello, ${name}!`);
 });
-  
 app.listen(3000, () => {
   console.log('Serveur démarré sur http://localhost:3000');
 });
